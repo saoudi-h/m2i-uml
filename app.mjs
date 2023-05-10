a65826f796bd678acc06e6de809046846aefef6d
Hakim Saoudi <68503582+saoudi-h@users.noreply.github.com>
2023-05-10 12:29:25 +0200

    init project

---
+++ app.mjs
@@ -0,0 +1,58 @@
+import cors from "cors";
+import csrf from "csurf";
+import chalk from "chalk";
+import crypto from "crypto";
+import helmet from "helmet";
+import express from "express";
+import config from "config";
+import session from "express-session";
+
+import "./loadEnvironment.mjs";
+
+
+const app = express();
+
+
+const PORT = config.app.port;
+const link = `http://${config.app.host}:${PORT}`;
+const secretSession = crypto.randomBytes(64).toString("hex");
+
+// Configuration de la session
+app.use(
+  session({
+    secret: secretSession,
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+      secure: false,
+      httpOnly: false,
+      maxAge: 24 * 60 * 60 * 1000,
+      sameSite: false,
+    },
+  })
+);
+
+app.use(csrf({}));
+app.use(
+  cors({
+    origin: true,
+    credentials: true,
+  })
+);
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(helmet());
+
+// services
+// app.use("/contact", contact);
+// app.use("/csrf", csrfToken);
+
+// Démarrer le serveur
+app.listen(PORT, () => {
+  console.log(`_`.repeat(60));
+  console.log(" ");
+  console.log(`Serveur en cours d'exécution sur le port ${PORT}`);
+  console.log(`Lien : ${chalk.blue(link)}`);
+  console.log(" ");
+  console.log("Server started.");
+});
---
+++ config/development.cjs
@@ -0,0 +1,22 @@
+require('dotenv').config();
+
+module.exports = {
+    app: {
+        port: 5000,
+        host: 'localhost',
+    },
+    db: {
+        host: '127.0.0.1',
+        port: '27017',
+        name: '',
+        logLevel: "",
+        userName: '',
+        userPwd: ''
+    },
+    client: {
+        url: 'http://localhost:3000'
+    },
+    session:{
+        maxMessages:10,
+    }
+}
